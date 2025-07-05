82e241ff26ab47f860b8d80c700aa08392765501
Openmusicacademy <eloallende.openmusicacademy@gmail.com>
2025-07-05 06:24:10 +0000

    vuelve dos veces atras

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -118,7 +118,7 @@ function DashboardHeader() {
         <Link 
             href="/dashboard" 
             aria-label={t('dashboard.sidebar.home')}
-            className="w-20 h-20 flex-shrink-0 transition-transform duration-300 ease-in-out hover:scale-110 active:scale-95 md:absolute md:left-[52%] md:-translate-x-1/2"
+            className="w-20 h-20 flex-shrink-0 transition-transform duration-300 ease-in-out hover:scale-110 active:scale-95 md:absolute md:left-1/2 md:-translate-x-1/2"
         >
             <Logo className="w-full h-full" />
         </Link>
