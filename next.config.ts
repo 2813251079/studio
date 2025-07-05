dee0c4194dad94950e3d0c02c198a4cbfddece1c
Openmusicacademy <eloallende.openmusicacademy@gmail.com>
2025-07-05 16:33:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,2 @@
-
 // This file is intentionally left blank. 
 // The configuration has been moved to next.config.cjs to resolve a startup issue.
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,7 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
+	],
   theme: {
     container: {
       center: true,
