c4153c391f2d7929e644126ceb09445cf0b4d6aa
Openmusicacademy <eloallende.openmusicacademy@gmail.com>
2025-07-07 02:44:08 +0000

    el circulo mas pqueño

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -14,17 +14,17 @@ const Logo = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
         <stop offset="100%" stopColor="#F65B36" />
       </radialGradient>
       <linearGradient id="textGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stopColor="#F65B36" />
-        <stop offset="50%" stopColor="#FDB813" />
-        <stop offset="100%" stopColor="#2ca064" />
+          <stop offset="0%" stopColor="#2ca064" />
+          <stop offset="50%" stopColor="#F65B36" />
+          <stop offset="100%" stopColor="#FDB813" />
       </linearGradient>
     </defs>
-    <g transform="translate(15, 6) scale(0.9)">
+    <g transform="translate(15, 6) scale(0.8)">
       {/* Guitar Body */}
-      <circle cx="68" cy="70" r="35" fill="url(#sunsetGradient)" />
+      <circle cx="68" cy="70" r="30" fill="url(#sunsetGradient)" />
 
       {/* Guitar Headstock & Neck */}
-      <g transform="translate(37, 10)">
+      <g transform="translate(37, -10)">
           {/* Headstock */}
           <path d="M 5 0 L -5 0 L -5 40 C -15 50, 5 55, 5 40 Z" fill="hsl(var(--primary))" />
           {/* Neck */}
@@ -56,7 +56,7 @@ const Logo = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
       </g>
 
       {/* Pentagram and F-Clef */}
-      <g transform="translate(44, 65) scale(1.1)">
+      <g transform="translate(44, 60) scale(1.1)">
           <g stroke="hsl(var(--muted-foreground))" strokeWidth="1.5">
             <path d="M 0 0 H 55" />
             <path d="M 0 8 H 55" />
